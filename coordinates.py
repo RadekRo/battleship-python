e859b620fdbd73d0406e785893ee33f5f9961e79
Radek Rocławski <r.roclawski@ankara.pl>
2023-02-20 14:11:51 +0100

    Created function names and description of needed functionality

---
+++ coordinates.py
@@ -0,0 +1,27 @@
+# coordinates = {}
+# create a dictionary with all the potential alphabet letters as keys and attach index numbers as its values.
+
+def get_human_ship_coordinates(board):
+    # should return a list of human ship coordinates to place on board
+    # like ([5, 2], [5, 3]) for a two sails ship or ([5, 5]) for a one sail ship.
+    # use coordinates dictionary to change letters to numbers.
+    # All the values returned must be integers! [row, column]
+    pass
+
+def get_ai_random_ship_coordinates(board):
+    # should return a list of ai ship coordinates to place on board
+    # like ([5, 2], [5, 3]) for a two sails ship or ([5, 5]) for a one sail ship.
+    # make it as universal as possible for for ex. larger board, etc.
+    # use coordinates dictionary to change letters to numbers.
+    # All the values returned must be integers! [row, column]
+    pass
+
+def get_human_shot_coordinates(board):
+    # should return two integers.
+    # first for a row, second for a columnn.
+    pass
+
+def get_ai_random_shot_coordinates(board):
+    # should return two integers.
+    # first for a row, second for a column.
+    pass
