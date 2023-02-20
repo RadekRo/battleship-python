ce8a81a8262ad39055a80958dbc37d0f01985602
Radek Rocławski <32206594+RadekRo@users.noreply.github.com>
2023-02-20 11:49:53 +0100

    Created base import comment, empty main function and two constants according to game mode. (#1)

---
+++ battleship.py
@@ -0,0 +1,10 @@
+# from board import <nazwy funkcji>
+# from coordinates import <nazwy funkcji>
+# from menu import <nazwy funkcji>
+
+HUMAN_VS_HUMAN = 1
+HUMAN_VS_RANDOM_AI = 2
+
+def main():
+    # gameplay function
+    pass
