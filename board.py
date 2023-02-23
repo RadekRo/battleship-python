2d42ad1efb31951040eae1c01db6ab7a111837e4
AdrianPaszek <adrian.paszek@gmail.com>
2023-02-23 23:27:11 +0100

    Empty Board

---
+++ board.py
@@ -0,0 +1,31 @@
+def get_empty_board():
+    empty_board = [['~', '~', '~', '~', '~'],
+                   ['~', '~', '~', '~', '~'],
+                   ['~', '~', '~', '~', '~'],
+                   ['~', '~', '~', '~', '~'],
+                   ['~', '~', '~', '~', '~']]
+    return empty_board
+def display_board(board):
+
+    player_one = input("Nick gracza \n• ")
+    player_one = player_one.upper()
+    print(" ")
+    print("    1   2   3   4   5")
+    print("  ","---+---+---+---+---")
+    print("A", "|", board[0][0], "|", board[0][1], "|", board[0][2], "|", board[0][3], "|", board[0][4], "|")
+    print("  ","---+---+---+---+---")
+    print("B", "|", board[1][0], "|", board[1][1], "|", board[1][2], "|", board[0][3], "|", board[0][4], "|")
+    print("  ","---+---+---+---+---")
+    print("C", "|", board[2][0], "|", board[2][1], "|", board[2][2], "|", board[0][3], "|", board[0][4], "|")
+    print("  ","---+---+---+---+---")
+    print("D", "|", board[3][0], "|", board[3][1], "|", board[3][2], "|", board[0][3], "|", board[0][4], "|")
+    print("  ","---+---+---+---+---")
+    print("E", "|", board[4][0], "|", board[4][1], "|", board[4][2], "|", board[0][3], "|", board[0][4], "|")
+    print("  ","---+---+---+---+---")
+
+empty_board = get_empty_board()
+print(empty_board)
+
+
+
+display_board(empty_board)
