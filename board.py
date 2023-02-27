d78ca1007bab431e71bd1ee6dd1758c93a10bf9a
RadekRo <r.roclawski@ankara.pl>
2023-02-27 22:48:25 +0100

    Changed get_empty_board to create dictionary with coords

--- board.py
+++ board.py
@@ -1,10 +1,14 @@
-def get_empty_board():
-    empty_board = [['~', '~', '~', '~', '~'],
-                   ['~', '~', '~', '~', '~'],
-                   ['~', '~', '~', '~', '~'],
-                   ['~', '~', '~', '~', '~'],
-                   ['~', '~', '~', '~', '~']]
+import string
+
+def get_empty_board(width, height):
+    empty_board = {}
+    for row in range(0, height):
+        letter = string.ascii_uppercase[row]
+        for column in range(0, width):
+            coordination = letter + str(column + 1)
+            empty_board[coordination] = "~"
     return empty_board
+    
 def display_board(board):
 
     player_one = input("Nick gracza \n• ")
