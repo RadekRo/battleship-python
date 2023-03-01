3f1cbb3d870f3acace6989cbc6c6bae02bfea39d
Radek Rocławski <r.roclawski@ankara.pl>
2023-03-01 16:08:23 +0100

    Added positioning phase loop to the main file

--- battleship.py
+++ battleship.py
@@ -9,6 +9,9 @@ BOARD_HEIGHT = 5
 def main():
     # gameplay function
     game_mode = menu()
+    positioning_phase = "active"
+    while positioning_phase == "active":
+     pass
     while game_mode == "active":
-        break
+     pass   
 main()
