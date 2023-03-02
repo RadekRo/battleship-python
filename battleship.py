73104fcca2701902723981755f272db37dc2ba83
RadekRo <r.roclawski@ankara.pl>
2023-03-02 20:53:45 +0100

    Created function for ship base according to board size

--- battleship.py
+++ battleship.py
@@ -5,9 +5,18 @@ from menu import menu
 HUMAN_VS_HUMAN = 1
 BOARD_SIZE = 5
 
+def get_ship_base(size):
+    three_sails = int(size * 0.2)
+    two_sails = int((size - three_sails) * 0.4)
+    one_sail = size - two_sails - three_sails
+    return [three_sails, two_sails, one_sail]
+
 def positioning_phase():
     board = get_empty_board(BOARD_SIZE)
-    print(board)
+    ship_quantity = get_ship_base(BOARD_SIZE)
+    while
+    display_board(board, BOARD_SIZE)
+
 
 def main():
     # gameplay function
@@ -16,4 +25,5 @@ def main():
     player_two = positioning_phase()
     #while game_mode == "active":
     # pass   
-main()
+#main()
+print(get_ship_base(5))
